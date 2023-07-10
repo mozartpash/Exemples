0cbc9507b0ae7a56881301241a787cf7e2ddd558
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-10 17:40:03 +0300

    Lection2

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//             0, 1, 2, 3, 4, 5, 6, 7, 8
+int[] array = {21,32,34,54,15,610,17,80,19};
+// array[0] = 12; // меняем значение с 21 на 12 в массиве с элементом с индексом 0 
+// Console.WriteLine(array[0]);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
+
+
---
+++ Example011_MethodLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void Fillarray(int[] collection)
+{
+    int length = collection.Length;// Получаем длинну массива по имени collection
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)//void - это метод, который не возвращает данные. При этом мы не ставим return
+{
+    int cout = col.Length;
+    int position = 0;
+    while(position < cout)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;// Если искомого элемента нет, то позиция вернёт на -1
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];// new int[10] Создали новый массив, в котором будет 10 элементов
+Fillarray(array);// заполнил массив
+PrintArray(array);// Напечатал массив
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
