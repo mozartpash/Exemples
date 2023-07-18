77ce47efaecb1250e46683b8a198ebff3ae5936d
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-18 19:15:14 +0300

    End of lection3

---
+++ Example012_MethodText/Program.cs
@@ -0,0 +1,34 @@
+﻿//Работа с текстом
+//Дан текст. В тексте все пробелы нужно заменаить чёрточками, маленькие буквы "к" заменить большими "К",
+//а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде"
+            + "вы бы взяли приступом согласие пруссого короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwery"
+//            01234
+//s[3] //- r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;//Присваевание пустой строки
+
+    int length = text.Length;// показываем кол-во символов в строке
+    for (int i = 0; i < length; i++)// Спомощью цикла for пробигаемся от 0-го символа, до конца строки
+    {
+        if(text[i]==oldValue) result = result + $"{newValue}";//Если символ в тексте = oldValue, то в result положим новое значение в виде строки {newValue}
+        else result = result + $"{text[i]}";//если нет совпадений, то добавлеем символ, корорый был
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
---
+++ Example013Massiv/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1, 1 };// создаём массив
+
+void PrintArray(int[] array)// В качестве аргумента приходим массив
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");// Выводим на экран через пробел
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)// пробегаемся по всей доинне массива
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];// Присваеваем временную посицию
+        array[i] = array[minPosition];//Происходит обмен двух переменных местами
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
